d5004c446fccd30f7cd52a6438e35a4320ada873
Andrzej Gebala <andrzej.gebala35@gmail.com>
2021-10-07 14:18:27 +0200

    dodanie switch na dzień tygodnia

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const currentDay = document.querySelector('.current-day')
-const funFact = document.querySelector('.fun-fact')
+const currentDay = document.querySelector('.current-days')
+const funFact = document.querySelector('.fun-facts')
 
 const facts = [
 	'Krokodyl nie potrafi wystawić języka.',
@@ -11,4 +11,30 @@ const facts = [
 	'Chińczycy w ciągu roku zużywają około 80 miliardów pałeczek.',
 	'Żeby wejść na Wieżę Eiffla trzeba pokonać aż 1710 stopni.'
 ]
+const date = new Date();
+const day = date.getDay();
+switch(day){
+	case 1:
+		currentDay.textContent = 'poniedziałek'
+		break;
+	case 2:
+		currentDay.textContent = 'wtorek'
+		break;
+	case 3:
+		currentDay.textContent = 'środa'
+		break;
+	case 4:
+		currentDay.textContent = 'czwartek'
+		break;
+	case 5:
+		currentDay.textContent = 'piątek'
+		break;	
+	case 6:
+	currentDay.textContent = 'sobota'
+	break;	
+	default:
+		currentDay.textContent = 'niedziela'
+
+}
+
 
